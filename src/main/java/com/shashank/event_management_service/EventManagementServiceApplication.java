2bedb2e02c291d0c9fdbc4a2a117fd729bb9d80a
sscstack <shashanktech2@gmail.com>
2024-09-11 08:01:17 +0530

    Initialized Event Management Service Project

---
+++ src/main/java/com/shashank/event_management_service/EventManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shashank.event_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashank/event_management_service/EventManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashank.event_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
